8b529d9fedf150ea7539b0191b977d86f7772246
Shazad0304 <50071791+Shazad0304@users.noreply.github.com>
2021-09-23 20:29:46 +0500

    Update pagination.ts

--- src/pagination/pagination.ts
+++ src/pagination/pagination.ts
@@ -28,3 +28,36 @@ export async function getPaginatedResult(repo : any, paginaton : PaginationReque
   }
 
 }
+
+export async function getPaginatedResultWithFind(repo : any, paginaton : PaginationRequest,find : any): Promise<PaginatedResult>{
+    
+    let condition = "";
+    let keys = Object.keys(find);
+
+    if(keys.length > 1){
+        
+        for(let key of keys){
+            condition = condition + `${key} = ${typeof find[key] === 'string' ? `'${find[key]}'` : find[key]} ${key === keys[keys.length - 1] ? '' : ' and '}`
+        }
+    }
+    else{
+            condition =  `${keys[0]} = ${typeof find[keys[0]] === 'string' ? `'${find[keys[0]]}'` : find[keys[0]]}`;
+    }
+    
+
+    const skippedItems = (paginaton.page - 1) * paginaton.limit;
+    const totalCount = await repo.count(find);
+    const items = await repo.createQueryBuilder()
+    .where(condition)
+    .offset(skippedItems)
+    .limit(paginaton.limit)
+    .getMany()
+
+  return {
+    totalCount,
+    page: paginaton.page,
+    limit: paginaton.limit,
+    data: items,
+  }
+
+}
